2597733aa83e518d88fc5504cc83794d8802cf67
Mr Bonnano <lucasbonnano@gmail.com>
2019-11-02 23:03:53 -0300

    Atualizando REPO

---
+++ EstruturaSequencial/5exercicio.py
@@ -0,0 +1,15 @@
+print('Faça um Programa que converta metros para centímetros.')
+
+
+
+def main():
+	_soma = float(input('Digite o Valor a ser Calculado: '))
+	_centimetros = 100
+
+	print('cm ',_soma * _centimetros)
+
+
+main()
+
+
+
---
+++ tabuada.py
@@ -0,0 +1,20 @@
+#Esse Codigo foi criado para auxiliar na aprendizagem de Python 
+# Versao ultilizada Foi Python3.7
+
+
+
+def main():
+	p1 = int(input('Digite a soma para exibir Tabela Tabuada: '))
+	while True:
+				
+
+		for i in range(1,11):
+				
+			somar = p1 * i
+
+			print('Numero: ', p1, '* ', i, ' = ', somar)
+
+
+		break	
+main()
+	
